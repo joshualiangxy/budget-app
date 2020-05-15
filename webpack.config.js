c573f7b2ca01f2451c2bd24211c1873d8e135ce7
Joshua Liang XingYa <joshualiang.xy@gmail.com>
2020-05-15 15:29:17 +0800

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,46 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: "development",
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === "production";
+    const MiniCSSExtract = new MiniCssExtractPlugin("styles.css");
+
+    return {
+        mode: isProduction ? "production" : "development",
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            MiniCSSExtract
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
